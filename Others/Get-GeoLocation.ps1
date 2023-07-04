88dc4b2916bdc5a901ed9aed1eec5c95351e185f
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2023-07-04 16:48:27 +0200

    Create Get-GeoLocation.ps1

---
+++ Others/Get-GeoLocation.ps1
@@ -0,0 +1,20 @@
+Function Get-GeoLocation {
+    Write-Host "Getting Geo-Location..."
+
+    $Uri = "https://ipinfo.io/json"
+    $Result = Invoke-RestMethod -Uri $Uri
+
+    $GeoLocation = [PSCustomObject]@{
+        Latitude    = $Result.loc.Split(',')[0]
+        Longitude   = $Result.loc.Split(',')[1]
+        City        = $Result.city
+        Country     = $Result.country
+        PostalCode  = $Result.postal
+    }
+
+    $GoogleMapsUrl = "https://www.google.com/maps?q=$($GeoLocation.Latitude),$($GeoLocation.Longitude)"
+    Start-Process $GoogleMapsUrl
+
+    return $GeoLocation
+    
+}Get-GeoLocation
