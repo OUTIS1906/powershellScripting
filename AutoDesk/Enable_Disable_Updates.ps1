6442462306a9c05706b58f9e4e08b01c2d037bdb
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2023-09-14 13:50:03 +0200

    Create Enable_Disable_Updates.ps1

---
+++ AutoDesk/Enable_Disable_Updates.ps1
@@ -0,0 +1,32 @@
+Function Enable_Disable_Updates{
+
+    Add-Type -AssemblyName PresentationFramework
+
+    $CurrnetUser = ((Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object UserName).UserName)
+    #$CurrentUserSID = (New-Object System.Security.Principal.NTAccount($CurrnetUser)).Translate([System.Security.Principal.SecurityIdentifier]).Value
+    
+    $CurrentUserSID = 'S-1-5-21-2047949552-857980807-821054962-504'
+    $registryFullPath = "registry::HKEY_USERS\$CurrentUserSID\SOFTWARE\Autodesk\ODIS"
+    
+    if(-not(Test-Path -Path $registryFullPath -PathType Container)){ 
+        New-Item -Path $registryFullPath -ItemType Directory -Force | Out-Null 
+    } 
+    
+    $Property = [bool](Get-ItemProperty -Path $registryFullPath -ErrorAction SilentlyContinue).DisableManualUpdateInstall
+    
+    switch ($Property) {
+        $true {
+            if(([System.Windows.MessageBox]::Show( "Enable updates?", "Autodesk Updates [DISABLED]","YesNo", "Asterisk" )) -eq "yes"){
+                Remove-ItemProperty -Path $registryFullPath -Name DisableManualUpdateInstall -Force
+                [void] [System.Windows.MessageBox]::Show( "Updates Enabled!", "ODIS Updates", "OK", "None" )
+            }
+        }
+        $false {
+            if(([System.Windows.MessageBox]::Show( "Disable updates?", "Autodesk Updates [ENABLED]","YesNo", "Asterisk" )) -eq "yes"){
+                New-ItemProperty -Path $registryFullPath -Name DisableManualUpdateInstall -PropertyType DWORD -Value 2 -Force | Out-Null
+                [void] [System.Windows.MessageBox]::Show( "Updates Disabled!", "ODIS Updates", "OK", "None" )
+            }
+        } 
+    }
+
+}Enable_Disable_Updates
