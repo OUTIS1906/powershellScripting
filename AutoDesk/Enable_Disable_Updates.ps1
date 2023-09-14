840d2adfdaa9b12a03558c3751bc75bdc48f9425
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2023-09-14 13:53:55 +0200

    Update Enable_Disable_Updates.ps1

--- AutoDesk/Enable_Disable_Updates.ps1
+++ AutoDesk/Enable_Disable_Updates.ps1
@@ -17,12 +17,14 @@ Function Enable_Disable_Updates{
     switch ($Property) {
         $true {
             if(([System.Windows.MessageBox]::Show( "Enable updates?", "Autodesk Updates [DISABLED]","YesNo", "Asterisk" )) -eq "yes"){
+                Get-Process *AdskAccess* | ForEach-Object { $_.Kill() }
                 Remove-ItemProperty -Path $registryFullPath -Name DisableManualUpdateInstall -Force
                 [void] [System.Windows.MessageBox]::Show( "Updates Enabled!", "ODIS Updates", "OK", "None" )
             }
         }
         $false {
             if(([System.Windows.MessageBox]::Show( "Disable updates?", "Autodesk Updates [ENABLED]","YesNo", "Asterisk" )) -eq "yes"){
+                Get-Process *AdskAccess* | ForEach-Object { $_.Kill() }
                 New-ItemProperty -Path $registryFullPath -Name DisableManualUpdateInstall -PropertyType DWORD -Value 2 -Force | Out-Null
                 [void] [System.Windows.MessageBox]::Show( "Updates Disabled!", "ODIS Updates", "OK", "None" )
             }
