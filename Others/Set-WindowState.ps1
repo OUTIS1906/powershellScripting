08b257694533e3013517be7fc221c013d1c0be76
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2024-03-26 15:04:43 +0100

    Create Set-WindowState.ps1

---
+++ Others/Set-WindowState.ps1
@@ -0,0 +1,41 @@
+function Set-WindowState {
+    param(
+        [Parameter()]
+        [ValidateSet('FORCEMINIMIZE', 'HIDE', 'MAXIMIZE', 'MINIMIZE', 'RESTORE', 
+                     'SHOW', 'SHOWDEFAULT', 'SHOWMAXIMIZED', 'SHOWMINIMIZED', 
+                     'SHOWMINNOACTIVE', 'SHOWNA', 'SHOWNOACTIVATE', 'SHOWNORMAL')]
+        [Alias('Style')]
+        [String] $State = 'SHOW',
+        
+        [Parameter(ValueFromPipelineByPropertyname='True')]
+        [System.IntPtr] $MainWindowHandle = (Get-Process –id $pid).MainWindowHandle
+    
+    )
+    
+    $WindowStates = @{
+        'FORCEMINIMIZE'   = 11
+        'HIDE'            = 0
+        'MAXIMIZE'        = 3
+        'MINIMIZE'        = 6
+        'RESTORE'         = 9
+        'SHOW'            = 5
+        'SHOWDEFAULT'     = 10
+        'SHOWMAXIMIZED'   = 3
+        'SHOWMINIMIZED'   = 2
+        'SHOWMINNOACTIVE' = 7
+        'SHOWNA'          = 8
+        'SHOWNOACTIVATE'  = 4
+        'SHOWNORMAL'      = 1
+    }
+        
+    $Win32ShowWindowAsync = Add-Type –memberDefinition @"
+[DllImport("user32.dll")] 
+public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow); 
+"@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
+    
+    $Win32ShowWindowAsync::ShowWindowAsync($MainWindowHandle, $WindowStates[$State]) | Out-Null
+    Write-Verbose ("Set Window State on '{0}' to '{1}' " -f $MainWindowHandle, $State)
+    
+    Set-Alias -Name 'Set-WindowStyle' -Value 'Set-WindowState'
+    
+}
