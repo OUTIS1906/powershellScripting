64cff59b66a54daddd2ece883812f373a483610d
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2024-03-29 11:49:55 +0100

    Update Get-WindowsCPUArchitecutre.ps1

--- WindowsDesktop/Get-WindowsCPUArchitecutre.ps1
+++ WindowsDesktop/Get-WindowsCPUArchitecutre.ps1
@@ -2,33 +2,37 @@ function Get-WindowsCPUArchitecutre {
     [CmdletBinding()]
     param (
         [Parameter(HelpMessage = "Provide remote computer name (default is current machine)")]
-        [string]$ComputerName = $env:computername
+        [string[]]$ComputerName = $env:computername
     )
 
     if($PSEdition -ne 'Desktop') { Write-Warning "Powershell Code doesn't support WMI-Object class."; return;; }
 
-    try{
-        $cpuInfo = Get-WmiObject -Class Win32_Processor -ComputerName $ComputerName
-        $osInfo  = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $ComputerName
-
-        return [pscustomobject]@{
-            "CPU Name"             = $cpuInfo.Name
-            "Number of Cores"      = $cpuInfo.NumberOfCores
-            "CPU Architecture"     = @{
-                0  = "x86"
-                5  = "ARM"
-                6  = "ia64"
-                9  = "x64"
-                12 = "ARM64"
-            }[[int]$cpuInfo.Architecture]
-            "Windows Architecture" = $osInfo.OSArchitecture
+    foreach($computer in $ComputerName)
+    {
+        try{
+            $cpuInfo = Get-WmiObject -Class Win32_Processor -ErrorAction Stop -ComputerName $computer 
+            $osInfo  = Get-WmiObject -Class Win32_OperatingSystem -ErrorAction Stop -ComputerName $computer
+    
+            [pscustomobject]@{
+                Computer        = $computer
+                CpuName         = $cpuInfo.Name
+                NumberOfCores   = $cpuInfo.NumberOfCores
+                CpuArchitecture = @{
+                    0  = "x86"
+                    5  = "ARM"
+                    6  = "ia64"
+                    9  = "x64"
+                    12 = "ARM64"
+                }[[int]$cpuInfo.Architecture]
+                WindowsArchitecture = $osInfo.OSArchitecture
+            }
         }
-    }
-    catch {
-        Write-Warning "Failed to retrieve information from $computername"
+        catch {
+            Write-Warning "Failed to retrieve information from $computer"
+        }
     }
 } 
 
 # Example usage:
 # Get-WindowsCPUArchitecutre
-# Get-WindowsCPUArchitecutre -ComputerName REMOTE-PC
+# Get-WindowsCPUArchitecutre -ComputerName REMOTE-PC1, REMOTE-PC2 | Format-List -GroupBy Computer
