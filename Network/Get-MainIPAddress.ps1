af2d602d14996d7f006ab80f23b0388436eaef28
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2024-03-29 02:40:56 +0100

    Create Get-MainIPAddress.ps1

---
+++ Network/Get-MainIPAddress.ps1
@@ -0,0 +1,41 @@
+Function Get-MainIPAddress {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$IPAddress = ""
+    )
+
+    try{
+        $result = Invoke-RestMethod -Method Get -Uri "http://ip-api.com/json/$IPAddress"
+
+        $outputObject = [PSCustomObject]@{}
+        if ($result.status -eq 'fail') {
+            $outputObject | Add-Member -NotePropertyName 'Status'           -NotePropertyValue $result.status
+            $outputObject | Add-Member -NotePropertyName 'Message'          -NotePropertyValue $result.message
+            $outputObject | Add-Member -NotePropertyName 'Query'            -NotePropertyValue $result.query
+        } else {
+            $outputObject | Add-Member -NotePropertyName 'Status'           -NotePropertyValue $result.status
+            $outputObject | Add-Member -NotePropertyName 'Country'          -NotePropertyValue $result.country
+            $outputObject | Add-Member -NotePropertyName 'CountryCode'      -NotePropertyValue $result.countryCode
+            $outputObject | Add-Member -NotePropertyName 'Region'           -NotePropertyValue $result.region
+            $outputObject | Add-Member -NotePropertyName 'RegionName'       -NotePropertyValue $result.regionName
+            $outputObject | Add-Member -NotePropertyName 'City'             -NotePropertyValue $result.city
+            $outputObject | Add-Member -NotePropertyName 'ZIP'              -NotePropertyValue $result.zip
+            $outputObject | Add-Member -NotePropertyName 'Latitude'         -NotePropertyValue $result.lat
+            $outputObject | Add-Member -NotePropertyName 'Longitude'        -NotePropertyValue $result.lon
+            $outputObject | Add-Member -NotePropertyName 'Timezone'         -NotePropertyValue $result.timezone
+            $outputObject | Add-Member -NotePropertyName 'ISP'              -NotePropertyValue $result.isp
+            $outputObject | Add-Member -NotePropertyName 'Organization'     -NotePropertyValue $result.org
+            $outputObject | Add-Member -NotePropertyName 'AutonomousSystem' -NotePropertyValue $result.as
+            $outputObject | Add-Member -NotePropertyName 'Query'            -NotePropertyValue $result.query
+        }
+    
+        $outputObject
+    }
+    catch {
+        Write-Warning "Failed to invoke web-request."
+    }
+}
+
+# Example usage:
+# Get-MainIPAddress
