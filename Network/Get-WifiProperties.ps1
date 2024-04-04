6d8d3292ad374c04add0e1854cef692833cc4038
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2024-04-04 13:24:08 +0200

    Create Get-WifiProperties.ps1

---
+++ Network/Get-WifiProperties.ps1
@@ -0,0 +1,28 @@
+Function Get-WifiProperties {
+
+    [System.Threading.Thread]::CurrentThread.CurrentCulture   = 'en-US'
+    [System.Threading.Thread]::CurrentThread.CurrentUICulture = 'en-US'
+    
+    $showInterface = & netsh wlan show interface
+
+    $psObject = [PSCustomObject]@{}
+    
+    foreach ($line in $showInterface) {
+            
+        $pattern = "^\s*([a-zA-Z\s-]+)\s*:\s*(.*?)\s*$"
+        $matches = [regex]::Matches($line, $pattern)
+    
+        if ($matches.Success) {
+    
+            $key   = $matches.Groups[1].Value.Trim()
+            $value = $matches.Groups[2].Value.Trim()
+    
+            $psObject | Add-Member -MemberType NoteProperty -Name $key -Value $value
+        }
+    }
+
+    return $psObject
+}
+
+## Example usage:
+# Get-WifiProperties
