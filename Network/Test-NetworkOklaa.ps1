77987391a7bf0e9622bc3545eae607e36719ecc3
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2024-05-29 15:15:06 +0200

    Create Test-NetworkOklaa.ps1

---
+++ Network/Test-NetworkOklaa.ps1
@@ -0,0 +1,66 @@
+Function Test-NetworkOklaa {
+    
+    Write-Host "Gathering information..." -NoNewline
+
+    $downloadUrl     = "https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-win64.zip"
+    $destiantionZip  = [io.path]::Combine($env:TEMP,'ookla-speedtest-1.2.0-win64.zip')
+    $destiantionPath = [io.path]::Combine($env:TEMP,'ookla-speedtest-1.2.0-win64')
+    
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($downloadUrl,$destiantionZip)
+    $webClient.Dispose()
+
+    Add-Type -Assembly System.IO.Compression.Filesystem
+
+    if ([io.directory]::Exists($destiantionPath)) {
+        [io.directory]::Delete($destiantionPath,$True)
+    }
+    [IO.Compression.Zipfile]::ExtractToDirectory($destiantionZip, $destiantionPath)
+
+    $filePath = [IO.Directory]::EnumerateFiles($destiantionPath,'speedtest.exe')
+
+    if($null -ne $filePath) {
+
+        # $Result = & $filePath --accept-gdpr --accept-license --format=json | ConvertFrom-Json
+
+        $processInfo = @{
+            FileName               = $filePath
+            Arguments              = "--accept-gdpr --accept-license --format=json-pretty --progress=no"
+            RedirectStandardOutput = $true
+            RedirectStandardError  = $true
+            UseShellExecute        = $false
+            CreateNoWindow         = $true
+        }
+        
+        $process = [System.Diagnostics.Process]::Start($processInfo)
+        
+        $stdOutput = $process.StandardOutput.ReadToEnd()
+        $stdError  = $process.StandardError.ReadToEnd()
+        
+        $process.WaitForExit()
+        
+        if ($process.ExitCode -eq 0) {
+            $Result = $stdOutput | ConvertFrom-Json -ErrorAction SilentlyContinue
+
+            Write-Host "Done." -NoNewline
+            return ([PSCustomObject]@{
+                DownloadSpeed = [math]::Round($Result.download.bandwidth / 1000000 * 8, 2)
+                UploadSpeed   = [math]::Round($Result.upload.bandwidth / 1000000 * 8, 2)
+                PacketLoss    = [math]::Round($Result.packetLoss)
+                ISP           = $Result.isp
+                ExternalIP    = $Result.interface.externalIp
+                InternalIP    = $Result.interface.internalIp
+                UsedServer    = $Result.server.host
+                URL           = $Result.result.url
+                Jitter        = [math]::Round($Result.ping.jitter)
+                Latency       = [math]::Round($Result.ping.latency)    
+            })
+        }
+        else {
+            Write-Warning "Error:`n$stdError"
+        } 
+    }
+} 
+
+# Example usage:
+# Test-NetworkOklaa
