f907f6140f8d54ab70772d20966706dc982391d8
OUTIS1906 <48081910+OUTIS1906@users.noreply.github.com>
2024-06-11 17:49:40 +0200

    Create Get-WindowsCredentials.ps1

---
+++ WindowsDesktop/Get-WindowsCredentials.ps1
@@ -0,0 +1,42 @@
+function Get-WindowsCredentials {
+    
+    $outputObject = (& cmdkey.exe /list 2>&1) -join "`n"
+    $pattern      = "Target:\s*(.*?)\r?\n\s*Type:\s*(.*?)\r?\n\s*User:\s*(.*?)\r?\n"
+
+    $matches = [regex]::Matches($outputObject, $pattern)
+    
+    $credObject = foreach ($match in $matches) {
+        [PSCustomObject]@{
+            Target = $match.Groups[1].Value
+            User   = $match.Groups[3].Value
+            Type   = $match.Groups[2].Value
+        }
+    }
+    return $credObject
+}
+
+# Example usage:
+# Get-WindowsCredentials
+
+function Remove-WindowsCredentials {
+
+    $outputObject = (& cmdkey.exe /list 2>&1) -join "`n"
+    $pattern      = "Target:\s*(.*?)\r?\n\s*Type:\s*(.*?)\r?\n\s*User:\s*(.*?)\r?\n"
+
+    $matches = [regex]::Matches($outputObject, $pattern)
+    
+    $credObject = foreach ($match in $matches) {
+        [PSCustomObject]@{
+            Target = $match.Groups[1].Value
+            User   = $match.Groups[3].Value
+            Type   = $match.Groups[2].Value
+        }
+    }
+
+    foreach ($item in $credObject | Out-GridView -OutputMode Multiple) {
+        & cmdkey.exe /del $item.Target  2>&1
+    }
+}
+
+# Example usage:
+# Remove-WindowsCredentials 
